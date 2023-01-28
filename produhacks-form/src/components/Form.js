9bedd5ade96d8779886384f80ba8be9dda117c5d
bennypc <52959626+bennypc@users.noreply.github.com>
2023-01-28 13:32:15 -0800

    sizing

--- produhacks-form/src/components/Form.js
+++ produhacks-form/src/components/Form.js
@@ -183,7 +183,7 @@ export default function Form() {
                   className="relative inline-block text-left float-right"
                 >
                   <div>
-                    <Menu.Button className="inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-100">
+                    <Menu.Button className="inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-3 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-100">
                       {selectedEvent ? selectedEvent : "Select Event"}
                       <ChevronDownIcon
                         className="-mr-1 ml-2 h-5 w-5"
